f19416a59a8b8f44ea81cc2887815721daee0f8a
Ahsan Virani <ahsan@Alnoors-MBP-2.hub>
2021-01-02 01:48:43 +1100

    Yputube API

--- Youtube_API/yt_api.py
+++ Youtube_API/yt_api.py
@@ -1,24 +1,35 @@
-
+# Importing libraries
 import re
+from datetime import timedelta
 from googleapiclient.discovery import build
 
+# Google API key
 api_key = 'AIzaSyCGjPH8WZXo9U5MifzHvIW0VTnNvlVt2MY'
 
+# API object of Youtube
 youtube = build('youtube', 'v3', developerKey=api_key)
 
+# REGEX compiler
+hours_pattern = re.compile(r'(\d+)H')
+minutes_pattern = re.compile(r'(\d+)M')
+seconds_pattern = re.compile(r'(\d+)S')
+
+# Requesting all video ID's from the playlist
 pl_request =  youtube.playlistItems().list(
 	part='contentDetails',
-	playlistId='PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS'
+	playlistId='PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS',
+	maxResults=50,
 )
 
+# Executing the request here
 pl_response = pl_request.execute()
 
-#print(pl_response)
-
+# Putting all video Id's in a list
 vid_ids = []
 for item in pl_response['items']:
 	vid_ids.append(item['contentDetails']['videoId'])
 
+# Requesting the video's via their respective ID's
 vid_request = youtube.videos().list(
 	part='contentDetails',
 	id=[','.join(vid_ids)]
@@ -26,7 +37,32 @@ vid_request = youtube.videos().list(
 
 vid_response = vid_request.execute()
 
+# Looping through all the videos and summing up total duration
+total_seconds = 0
 for item in vid_response['items']:
 	duration = item['contentDetails']['duration']
-	print(duration)
-	print()
+
+	hours = hours_pattern.search(duration)
+	minutes = minutes_pattern.search(duration)
+	seconds = seconds_pattern.search(duration)
+
+	hours = int(hours.group(1)) if hours else 0
+	minutes = int(minutes.group(1)) if minutes else 0
+	seconds = int(seconds.group(1)) if seconds else 0
+
+	video_seconds = timedelta(
+		hours=hours,
+		minutes=minutes,
+		seconds=seconds
+	).total_seconds()
+
+	total_seconds += video_seconds
+
+# Convert total seconds to proper time
+
+total_seconds = int(total_seconds)
+minutes, seconds = divmod(total_seconds, 60)
+hours, minutes = divmod(minutes, 60)
+
+# Printing out the total time
+print(f'{hours}:{minutes}:{seconds}')
